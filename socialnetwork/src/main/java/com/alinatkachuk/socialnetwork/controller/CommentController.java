829c3b57bf1c7b86f06585a02c5b4f269deb9dba
alinatkachuk <atkachuk2790@gmail.com>
2021-05-12 21:57:52 +0300

    Feature added methods for comment controller

--- socialnetwork/src/main/java/com/alinatkachuk/socialnetwork/controller/CommentController.java
+++ socialnetwork/src/main/java/com/alinatkachuk/socialnetwork/controller/CommentController.java
@@ -1,9 +1,29 @@
 package com.alinatkachuk.socialnetwork.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.alinatkachuk.socialnetwork.model.Comment;
+import com.alinatkachuk.socialnetwork.service.CommentServiceImpl;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/users")
 public class CommentController {
+
+    CommentServiceImpl commentServiceImpl;
+
+    @GetMapping(path="{userId}/posts")
+    public String createComment (@PathVariable("userId") Long userId,
+                                 @PathVariable("postId") Long postId,
+                                 Model model) {
+        model.addAttribute("comment", new Comment ());
+        return "createComment";
+    }
+
+    @PostMapping(path="{userId}/posts/new/comment")
+    public void savePost (@PathVariable("postId") Long postId,
+                          @PathVariable("comment") Comment comment,
+                          @PathVariable("userId") Long userId) {
+        commentServiceImpl.saveComment (postId, comment);
+    }
+
 }
