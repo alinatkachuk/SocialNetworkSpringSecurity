b473f1ea0a279fc8c1011631e2d45620a3b07b7c
alinatkachuk <atkachuk2790@gmail.com>
2021-05-24 23:15:28 +0300

    Fix bug UnsatisfiedDependencyException fixed

--- socialnetwork/src/main/java/com/alinatkachuk/socialnetwork/SocialNetworkApplication.java
+++ socialnetwork/src/main/java/com/alinatkachuk/socialnetwork/SocialNetworkApplication.java
@@ -2,9 +2,11 @@ package com.alinatkachuk.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableJpaRepositories
 @SpringBootApplication
 @RestController
 public class SocialNetworkApplication {
